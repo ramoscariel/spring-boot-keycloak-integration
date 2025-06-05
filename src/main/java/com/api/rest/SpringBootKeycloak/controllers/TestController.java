d3fa4a2df2ed5a72968f010d19e14e0f38cd3f2e
arielramos03 <103594586+ramoscariel@users.noreply.github.com>
2025-06-05 10:05:22 -0500

    clone repo

---
+++ src/main/java/com/api/rest/SpringBootKeycloak/controllers/TestController.java
@@ -0,0 +1,25 @@
+package com.api.rest.SpringBootKeycloak.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello-1")
+    @PreAuthorize("hasRole('admin_client_role')")
+    public String helloAdmin(){
+        return "Hello Sprig Boot With Keycloak with ADMIN";
+    }
+
+    @GetMapping("/hello-2")
+    @PreAuthorize("hasRole('client_user') or hasRole('client_admin')")
+    public String helloUser(){
+        return "Hello Sprig Boot With Keycloak with USER";
+    }
+
+
+    @GetMapping("/hello")
+    public String hello(){return "Hola mundo";}
+}
---
+++ src/main/java/com/api/rest/SpringBootKeycloak/controllers/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package com.api.rest.SpringBootKeycloak.controllers.dto;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Set;
+
+@Value
+@RequiredArgsConstructor
+@Builder
+@Getter
+public class UserDTO implements Serializable {
+
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private Set<String> roles;
+}
